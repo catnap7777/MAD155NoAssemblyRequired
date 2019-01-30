f828b7bdb79c1226e83ac73e8ff36ba9f70e77ea
kam <kmathes348@students.mchenry.edu>
2019-01-30 16:44:05 -0600

    Initial commit

---
+++ app/src/main/java/mad155/kmathes/mad155noassemblyrequired/DisplayTools.java
@@ -0,0 +1,13 @@
+package mad155.kmathes.mad155noassemblyrequired;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayTools extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_tools);
+    }
+}
